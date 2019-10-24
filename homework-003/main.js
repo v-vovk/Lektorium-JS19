07abfa47e8f46ae7a9d6c76bb7b10c8e7c17d6d2
Vlad Vovk <wolfykey@gmail.com>
2019-10-24 20:52:13 +0300

    complete homework-003

--- homework-003/main.js
+++ homework-003/main.js
@@ -1,30 +1,88 @@
 'use strict'
 
 class Vegetable {
-  constructor (health = 50, happiness = 50, weight = 84, muscle = 20, money = 1000) {
+  constructor (health = 50, happiness = 50, weight = 84, muscle = 20, money = 1000, depresion = 100, rip = false) {
     this.health = health
     this.happiness = happiness
     this.weight = weight
     this.muscle = muscle
     this.money = money
+    this.depresion = depresion
+    this.rip = rip
   }
 
-  // createDiv () {
-  //   let elem = document.createElement('div')
-  //   document.body.appendChild(elem)
-  //   let param = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontSize}px; text-align: ${this.textAlign}`
-  //   elem.style.cssText = param
-  // }
-
   drinkWithFriend () {
-    this.health -= 10
+    if (this.health === 0 || this.depresion >= 100) {
+      this.rip = true
+    }
+    if (this.health >= 10) {
+      this.health -= 10
+    }
     this.happiness += 20
-    this.weight -= 2
-    this.muscle -= 2
-    this.money -= 100
+    if (this.weight >= 30) {
+      this.weight -= 2
+    }
+    if (this.muscle >= 5) {
+      this.muscle -= 2
+    }
+    if (this.money >= 100) {
+      this.money -= 100
+    }
+    this.depresion -= 10
+  }
+
+  eat () {
+    if (this.health === 0 || this.depresion >= 100 || this.muscle === 0) {
+      this.rip = true
+    }
+    this.health += 10
+    this.happiness += 20
+    this.weight += 5
+    this.muscle += 1
+    if (this.money >= 100) {
+      this.money -= 50
+    }
+    this.depresion -= 10
+  }
+
+  walk () {
+    if (this.health === 0 || this.depresion >= 100 || this.muscle === 0) {
+      this.rip = true
+    }
+    this.health += 10
+    this.happiness += 10
+    if (this.weight >= 1) {
+      this.weight -= 1
+    }
+    this.muscle += 1
+    this.depresion -= 10
+  }
+
+  sleep () {
+    if (this.health === 0 || this.depresion >= 100 || this.muscle === 0) {
+      this.rip = true
+    }
+    this.health += 20
+    this.happiness += 30
+    this.depresion -= 10
+  }
+
+  died () {
+    if (this.health === 0 || this.muscle === 0 || this.depresion >= 100 || this.happiness <= 0) {
+      this.rip = true
+      console.log('твій друг-овоч помер')
+    } else {
+      this.rip = false
+    }
+  }
+
+  goAway () {
+    if (this.health <= 10 || this.muscle <= 5 || this.depresion >= 80 || this.happiness <= 10) {
+      console.log('твій друг-овоч втік')
+    }
   }
 }
 
-const yourFriend = new Vegetable(30, 20, 73, 13, 1500)
+const yourFriend = new Vegetable(30, 20, 73, 13, 1500, 23)
 
 console.log(yourFriend)
