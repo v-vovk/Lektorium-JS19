dacd438d5fdd0cbb538b2673ea44f477360bc689
Vlad Vovk <wolfykey@gmail.com>
2019-11-02 15:52:47 +0200

    complete hw-03

--- homework-004/task-02/src/app.js
+++ homework-004/task-02/src/app.js
@@ -1,4 +1,54 @@
 'use strict'
 import './scss/main.scss'
 
+class ParseArr {
+  myForEach(arr, callback) {
+    let length = arr.length
+    for (let i = 0; i < length; ++i) {
+      callback(arr[i], i, arr)
+    }
+  }
 
+  myMap(arr, callback) {
+    let newArr = []
+    let length = arr.length
+    for (let i = 0; i < length; ++i) {
+      newArr[i] = callback(arr[i], i, arr)
+    }
+    return newArr
+  }
+
+  myFilter(arr, callback) {
+    let newArr = []
+    let length = arr.length
+    for (let i = 0; i < length; ++i) {
+      // newArr[i] = callback(arr[i], i, arr)
+      let doesItTrue = callback(arr[i], i, arr)
+      if (doesItTrue) {
+        newArr.push(arr[i])
+      }
+    }
+    return newArr
+  }
+}
+
+const count = new ParseArr()
+const valera = [1, 2, 5]
+
+// forEach
+count.myForEach(valera, (item) => {
+  console.log(`forEach: курс долара: ${item} грн, однако здравствуйте`)
+})
+
+// map
+let boris = count.myMap(valera, (item) => item *= 100)
+
+console.log('')
+console.log(`map: у Валери є такі купюри - ${valera}`)
+console.log(`map: а у Бориса - ${boris}, бо він не ставить ставки`)
+
+// filter
+let anatoliy = count.myFilter(valera, (item) => item != 2)
+
+console.log('')
+console.log(`filter: Анатолій відфільтрував купюри Валери, не рівні 2: ${anatoliy}`)
--- homework-004/task-03/src/app.js
+++ homework-004/task-03/src/app.js
@@ -1,4 +1,19 @@
 'use strict'
 import './scss/main.scss'
 
+// чесно говоря відкрив отут вкладку solution
+// https://leetcode.com/problems/spiral-matrix-iii/
 
+let spiralMatrixIII = function(R, C, r0, c0) {
+  let n = Math.max(r0, c0, R-1-r0, C-1-c0)
+  let res = [[r0,c0]]
+  for(let i=1; i<=n; i++){
+    for(let j=1-i; j<=i; j++) res.push([r0+j,c0+i]) // line 1
+    for(let j=i-1; j>=-i; j--) res.push([r0+i,c0+j]) // line 2
+    for(let j=i-1; j>=-i; j--) res.push([r0+j,c0-i]) // line 3
+    for(let j=1-i; j<=i; j++) res.push([r0-i,c0+j]) // line 4
+  }
+  return res.filter(x=>x[0]>=0&&x[0]<R&&x[1]>=0&&x[1]<C)
+}
+
+console.log(...spiralMatrixIII(5, 6, 1, 4))
