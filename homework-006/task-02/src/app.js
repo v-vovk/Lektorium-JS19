cc63955895d0cea70679461bee67844e7a94bf65
Vlad Vovk <wolfykey@gmail.com>
2019-11-11 21:28:28 +0200

    update task2

--- homework-006/task-02/src/app.js
+++ homework-006/task-02/src/app.js
@@ -6,25 +6,28 @@ const area = document.querySelector('.tagging-wrapper')
 
 const onMouseMove = e => {
   e.preventDefault()
-  pin.style.top = e.offsetY + 'px'
-  pin.style.left = e.offsetX + 'px'
+
+  const boxArea = area.getBoundingClientRect()
+
+  pin.style.top = e.clientY - boxArea.y + 'px'
+  pin.style.left = e.clientX - boxArea.x + 'px'
 
   const pinClose = pin.querySelector('.delete')
   pinClose.classList.remove('hide')
 
-  console.log(e.offsetX)
-  console.log(e.offsetY)
+  console.log(e.clientX - boxArea.x)
+  console.log(e.clientY - boxArea.y)
 
-  if (e.offsetY > 512) pin.style.top = '512px'
-  if (e.offsetY < 0) pin.style.top = '0px'
+  if (e.clientY - boxArea.y > 512) pin.style.top = '512px'
+  if (e.clientY - boxArea.y < 0) pin.style.top = '0px'
 
-  if (e.offsetX > 512) pin.style.left = '512px'
-  if (e.offsetX < 0) pin.style.left = '0px'
+  if (e.clientX - boxArea.x > 512) pin.style.left = '512px'
+  if (e.clientX - boxArea.x < 0) pin.style.left = '0px'
 }
 
 const onMouseUp = e => {
   e.preventDefault()
-  area.removeEventListener('mousemove', onMouseMove)
+  area.removeEventListener('mousemove', onMouseMove(area))
   area.removeEventListener('mouseup', onMouseUp)
 
   const pinClose = pin.querySelector('.delete')
